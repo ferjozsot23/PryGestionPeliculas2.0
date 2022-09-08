d07a64e056b5b5c100a730b2c3d7307f1e9d4b43
matucho01 <cristopher.perez01@epn.edu.ec>
2022-09-08 10:33:51 -0500

    Toma tu main hp

--- src/main/java/TestConection.java
+++ src/main/java/TestConection.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
+
 public class TestConection {
     public static void main (String [] args) {
         System.out.println("Hola mundo");
+        JOptionPane.showMessageDialog(null, "Titulo csm", "Hola Bairon", JOptionPane.INFORMATION_MESSAGE);
     }
 }
