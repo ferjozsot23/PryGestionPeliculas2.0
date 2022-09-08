8c8b58e9c837dab95843b79745f66ed6a496ee31
Brynta2001 <110837963+Brynta2001@users.noreply.github.com>
2022-09-08 10:43:03 -0500

    h

--- src/main/java/TestConection.java
+++ src/main/java/TestConection.java
@@ -1,8 +1,44 @@
 import javax.swing.*;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class TestConection {
-    public static void main (String [] args) {
-        System.out.println("Hola mundo");
-        JOptionPane.showMessageDialog(null, "Titulo csm", "Hola Bairon", JOptionPane.INFORMATION_MESSAGE);
+    public static void main (String [] args) throws IOException {
+        JSONObject json = readJsonFromUrl("http://unisatelite.com/Movies/registroPelicula.php");
+        System.out.println(json.getJSONArray("data").get(0));
+
+        JSONObject json2 = (JSONObject) json.getJSONArray("data").get(0);
+        System.out.println(json2.get("id_pelicula"));
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
     }
 }
