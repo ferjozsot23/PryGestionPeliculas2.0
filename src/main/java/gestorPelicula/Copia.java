6d113010e19e1b4875b92190118bc98e5ad111e6
Mateo <promateo2001@gmail.com>
2022-09-08 12:16:53 -0500

    Clase Copia

--- src/main/java/gestorPelicula/Copia.java
+++ src/main/java/gestorPelicula/Copia.java
@@ -1,4 +1,38 @@
 package gestorPelicula;
 
+import java.util.ArrayList;
+
 public class Copia {
+
+    private String idCopia;
+    private boolean disponible;
+    private Pelicula pelicula;
+
+    public Copia(ArrayList<String> infoCopia){
+        this.idCopia = infoCopia.get(0);
+        this.disponible = Boolean.parseBoolean(infoCopia.get(1));
+    }
+    public String getIdCopia() {
+        return idCopia;
+    }
+
+    public void setIdCopia(String idCopia) {
+        this.idCopia = idCopia;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public Pelicula getPelicula() {
+        return pelicula;
+    }
+
+    public void setPelicula(Pelicula pelicula) {
+        this.pelicula = pelicula;
+    }
 }
