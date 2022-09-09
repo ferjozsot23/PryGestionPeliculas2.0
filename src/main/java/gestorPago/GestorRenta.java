1c6718c060a3b7c80122409c006abb9aa85c0fd2
Sebastián Cervantes <sebascerboni88@gmail.com>
2022-09-09 01:46:43 -0500

    Cambios en la clase Renta.java Creación GestorRenta

--- src/main/java/gestorPago/GestorRenta.java
+++ src/main/java/gestorPago/GestorRenta.java
@@ -13,18 +13,22 @@ public class GestorRenta {
     private Renta renta;
     private Recibo recibo;
     private Recibo multa;
+    private int dias;
     SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
     private AdapterRenta adaptadorRenta = new AdapterRenta();
 
-    //Creación de renta y guardado en la base de datos
-    /*public void agregarRenta(){
-        this.rentas.add(new Renta(new java.util.Date()));
-        adaptadorRenta.insertarRenta(new Renta(id, new java.util.Date()));
-    }*/
+    public void agregarRenta(){
+        adaptadorRenta.insertarRenta(renta);
 
-    public void agregarRenta(Date fechaInicio, String id_Cliente, int id_Copia){
-        adaptadorRenta.insertarRenta(new Renta(fechaInicio, id_Cliente,id_Copia));
+    }
+
+    public void crearRenta(Date fechaInicio, String id_Cliente, int id_Copia){
         renta = new Renta(fechaInicio, id_Cliente,id_Copia);
+    }
+
+    public void finalizarRenta(Date fechaFin){
+        renta.setFecha_entrega(fechaFin);
+        establecerDiasExtendidos(renta.getFecha_Inicio(),fechaFin);
     }
 
     public void obtenerInformacionRenta(int idRenta){
@@ -33,21 +37,17 @@ public class GestorRenta {
 
     public void finalizarRenta(int idRenta, Date fechaEntrega){
         ArrayList<String> rentas = adaptadorRenta.getRenta(String.valueOf(idRenta));
-        adaptadorRenta.actualizarRenta(new Renta());
-    }
 
+    }
+/*
     public void emitirRecibo(){
         this.recibo = new Recibo(adaptadorRenta);  //datos del cliente
 
     }
+*/
 
-    public void emitirMulta(){
-        this.multa = new Recibo(this.getDias_extendidos());
-    }
-
-    public void calcularDiasExcedidos(Date fechaEntrega){
-        int dias_extendidos= (int) ChronoUnit.DAYS.between((Temporal) getFecha_entrega(), (Temporal) fechaEntrega);
-
+    public void establecerDiasExtendidos(Date fechaEntrega, Date fechaInicio){
+        renta.setDias_extendidos(fechaEntrega.getDay() - fechaInicio.getDay());
     }
 
 }
